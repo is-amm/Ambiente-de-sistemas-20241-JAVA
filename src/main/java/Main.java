9e29b47e7098e1eef30fe0036db51dc9ac8970dd
is-amm <107645348+is-amm@users.noreply.github.com>
2024-02-15 03:08:13 +0000

    Soma de Números adicionada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,9 @@ public class Main {
   public static void main(String[] args) {
     TrocaDeVariaveis t = new TrocaDeVariaveis();
     t.TrocaDeValores();
+
+    SomaDeNumeros s = new SomaDeNumeros();
+    s.SomaDeNumeros();
   }
 
   // @Test
---
+++ src/main/java/SomaDeNumeros.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SomaDeNumeros {
+  Scanner prompt = new Scanner(System.in);
+  int n, num, soma = 0, i = 0;
+
+  public void SomaDeNumeros() {
+    System.out.print("Quantos numeros a serem somados: ");
+    n = prompt.nextInt();
+    while (i < n) {
+      i = i + 1;
+      System.out.println("Entre com o " + i + "° numero");
+      num = prompt.nextInt();
+      soma = soma + num;
+    }
+    System.out.println("A soma da: " + soma);
+  }
+}
